924c1bfed7f824186f5d707a53499943b6cbf0d6
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-05-27 10:46:52 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SP_Trigger_Challenge_Team_FatemaRihamZahra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
